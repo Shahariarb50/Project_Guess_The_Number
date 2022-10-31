eb0e3287491edd9cb12f35239a357bb2dc082f28
ridoy2112 <you@example.com>
2022-10-31 08:16:46 +0600

    Second Commit

--- script.js
+++ script.js
@@ -30,12 +30,14 @@ const playerScore = document.querySelector(
 );
 const highScore = document.querySelector("body .play-with-friend .high-score");
 const playAgain = document.querySelector("body .play-with-friend .play-again");
+const body = document.querySelector("body");
 // console.log(playerScore);
 // const randomNumber = Math.trunc(Math.random() * 2) + 1;
-let inputValue = 0;
+// let inputValue1 = 0;
+// let inputValue2 = 0;
 let score = 20;
 let playerHighScrore = 0;
-console.log(inputValue);
+// console.log(inputValue1);
 let playerByttonNumber = 0;
 const playerButtonBorder = function (border) {
   playerByttonNumber > 1
@@ -57,165 +59,140 @@ playerWithComputer.addEventListener("mouseover", function () {
     playerButtonBorder("none");
   });
 });
-// playAgain.addEventListener('click',function(){
-//     guess.textContent = 'Start Guess A Number Btween 1 to 20'
-//     playerScore.textContent = 'Score = 20'
-//     const randomNumber = Math.trunc(Math.random() * 2) + 1;
-//     player1Input.disabled = false
-//     player2Input.disabled = false
-// })
+const scoreValue = function (input1, input2, player) {
+  console.log(input1);
+  console.log(input2);
+  if (Number(input1) > Number(input2)) {
+    guess.textContent = "😑Your Number Is Low";
+  } else {
+    guess.textContent = "😑Your Number Is High";
+  }
+
+  score > 5 ? (score -= 5) : (score = 0);
+  if (score === 15) {
+    lifeFour.style.color = "#f0f8ff";
+    playerScore.textContent = `Score = ${score} ${player}`;
+    // input1 = 0;
+    // input2 = 0;
+  } else if (score === 10) {
+    lifeThree.style.color = "#f0f8ff";
+    playerScore.textContent = `Score = ${score} ${player}`;
+    // input1 = 0;
+    // input2 = 0;
+  } else if (score === 5) {
+    lifeTwo.style.color = "#f0f8ff";
+    playerScore.textContent = `Score = ${score} ${player}`;
+    // input1 = 0;
+    // input2 = 0;
+  } else {
+    lifeOne.style.color = "#f0f8ff";
+    screen.textContent = "Lost!";
+    screen.style.fontSize = "50px";
+    player1Input.type = "text";
+    guess.textContent = "Player 1 Lost The Game !";
+    playerScore.textContent = `Score = ${score} ${player}`;
+    highScore.textContent = `🥳Current High Score = ${score} ${player}`;
+    player1Input.disabled = true;
+    player2Input.disabled = true;
+    // input1 = 0;
+    // input2 = 0;
+  }
+};
+const playerSubmitFunction = function (playerSubmit, input1, input2, player) {
+  let inputValue1 = Number(input1.value);
+  if (!inputValue1) {
+    guess.textContent = "Empty Box Not Allow !";
+    input1.disabled = false;
+  } else {
+    input1.disabled = true;
+    input2.disabled = false;
+    playerSubmit.addEventListener("click", function () {
+      let inputValue2 = Number(input2.value);
+      if (inputValue1 === inputValue2) {
+        console.log(inputValue1);
+        console.log(inputValue2);
+        screen.textContent = inputValue1;
+        guess.textContent = "🎉You Guess The Correct Number !";
+        input1.type = "text";
+        input2.type = "text";
+        input1.disabled = true;
+        input2.disabled = true;
+        if (score > playerHighScrore) {
+          playerHighScrore = score;
+          highScore.textContent = `🥳Current High Score = ${playerHighScrore} ${player}`;
+        }
+      } else {
+        scoreValue(inputValue1, inputValue2, player);
+      }
+    });
+  }
+};
 playerWithFriend.addEventListener("click", function () {
   const randomNumber = Math.trunc(Math.random() * 2) + 1;
+  // const randomNumber = 1;
   allButton.style.display = "none";
   playWithFriend.style.display = "inline";
   life.style.color = "#ff005d";
   if (randomNumber === 1) {
     player2Input.disabled = true;
     player1Submit.addEventListener("click", function () {
-      inputValue = Number(player1Input.value);
-      console.log(inputValue);
-      if (!Number(player1Input.value)) {
-        guess.textContent = "Empty Box Not Allow !";
-        player1Input.disabled = false;
-      } else {
-        player1Input.disabled = true;
-        player2Input.disabled = false;
-        player2Submit.addEventListener("click", function () {
-          if (inputValue === Number(player2Input.value)) {
-            screen.textContent = inputValue;
-            guess.textContent = "🎉You Guess The Correct Number !";
-            player1Input.type = "text";
-            player2Input.type = "text";
-            player2Input.disabled = true;
-            if (score > playerHighScrore) {
-              playerHighScrore = score;
-              highScore.textContent = `🥳Current High Score = ${playerHighScrore} (Player2)`;
-            }
-          } else if (inputValue < Number(player2Input.value)) {
-            guess.textContent = "😑Your Number Is High";
-            player2Input.value = "";
-
-            score >= 5 ? (score -= 5) : (score = 0);
-            if (score === 15) {
-              lifeFour.style.color = "#f0f8ff";
-              playerScore.textContent = `Score = ${score} (Player1)`;
-            } else if (score === 10) {
-              lifeThree.style.color = "#f0f8ff";
-              playerScore.textContent = `Score = ${score} (Player1)`;
-            } else if (score === 5) {
-              lifeTwo.style.color = "#f0f8ff";
-              playerScore.textContent = `Score = ${score} (Player1)`;
-            } else {
-              lifeOne.style.color = "#f0f8ff";
-              player1Input.disabled = true;
-              screen.textContent = "Lost!";
-              screen.style.fontSize = "50px";
-              player1Input.type = "text";
-              guess.textContent = "Player 1 Lost The Game !";
-              playerScore.textContent = `Score = ${score} (Player2)`;
-              highScore.textContent = `🥳Current High Score = ${score} (Player2)`;
-            }
-          } else {
-            guess.textContent = "😑Your Number Is Low";
-            player2Input.value = "";
-            score >= 5 ? (score -= 5) : (score = 0);
-            if (score === 15) {
-              lifeFour.style.color = "#f0f8ff";
-              playerScore.textContent = `Score = ${score} (Player2)`;
-            } else if (score === 10) {
-              lifeThree.style.color = "#f0f8ff";
-              playerScore.textContent = `Score = ${score} (Player2)`;
-            } else if (score === 5) {
-              lifeTwo.style.color = "#f0f8ff";
-              playerScore.textContent = `Score = ${score} (Player2)`;
-            } else {
-              lifeOne.style.color = "#f0f8ff";
-              player1Input.disabled = true;
-              screen.textContent = "Lost!";
-              screen.style.fontSize = "50px";
-              player1Input.type = "text";
-              guess.textContent = "Player 1 Lost The Game !";
-              playerScore.textContent = `Score = ${score} (Player2)`;
-              highScore.textContent = `🥳Current High Score = ${playerHighScrore} (Player2)`;
-              player2Input.disabled = true;
-            }
-          }
-        });
-      }
+      playerSubmitFunction(
+        player2Submit,
+        player1Input,
+        player2Input,
+        "player2"
+      );
     });
   } else {
     player1Input.disabled = true;
     player2Submit.addEventListener("click", function () {
-      inputValue = Number(player2Input.value);
-      console.log(inputValue);
-      if (!Number(player2Input.value)) {
-        guess.textContent = "😒Empty Box Not Allow !";
-        // player1Input.disabled = true
-        player2Input.disabled = false;
-      } else {
-        player2Input.disabled = true;
-        player1Input.disabled = false;
-        player1Submit.addEventListener("click", function () {
-          if (inputValue === Number(player1Input.value)) {
-            screen.textContent = inputValue;
-            guess.textContent = "🎉You Guess The Correct Number !";
-            player1Input.type = "text";
-            player2Input.type = "text";
-            player1Input.disabled = true;
-            playerScore.textContent = `Score = ${score} (Player1)`;
-            if (score > playerHighScrore) {
-              playerHighScrore = score;
-              highScore.textContent = `🥳Current High Score = ${playerHighScrore} (Player1)`;
-            }
-          } else if (inputValue < Number(player1Input.value)) {
-            guess.textContent = "😑Your Number Is High";
-            player1Input.value = "";
-            score >= 5 ? (score -= 5) : (score = 0);
-            if (score === 15) {
-              lifeFour.style.color = "#f0f8ff";
-              playerScore.textContent = `Score = ${score} (Player1)`;
-            } else if (score === 10) {
-              lifeThree.style.color = "#f0f8ff";
-              playerScore.textContent = `Score = ${score} (Player1)`;
-            } else if (score === 5) {
-              lifeTwo.style.color = "#f0f8ff";
-              playerScore.textContent = `Score = ${score} (Player1)`;
-            } else {
-              lifeOne.style.color = "#f0f8ff";
-              player1Input.disabled = true;
-              screen.textContent = "Lost!";
-              screen.style.fontSize = "50px";
-              player2Input.type = "text";
-              guess.textContent = "Player 1 Lost The Game !";
-              playerScore.textContent = `Score = ${score} (Player1)`;
-              highScore.textContent = `🥳Current High Score = ${score} (Player1)`;
-            }
-          } else {
-            guess.textContent = "😑Your Number Is Low";
-            player1Input.value = "";
-            score >= 5 ? (score -= 5) : (score = 0);
-            if (score === 15) {
-              lifeFour.style.color = "#f0f8ff";
-              playerScore.textContent = `Score = ${score} (Player1)`;
-            } else if (score === 10) {
-              lifeThree.style.color = "#f0f8ff";
-              playerScore.textContent = `Score = ${score} (Player1)`;
-            } else if (score === 5) {
-              lifeTwo.style.color = "#f0f8ff";
-              playerScore.textContent = `Score = ${score} (Player1)`;
-            } else {
-              lifeOne.style.color = "#f0f8ff";
-              player1Input.disabled = true;
-              screen.textContent = "Lost!";
-              screen.style.fontSize = "50px";
-              player2Input.type = "text";
-              guess.textContent = "Player 1 Lost The Game !";
-              playerScore.textContent = `Score = ${score} (Player1)`;
-              highScore.textContent = `🥳Current High Score = ${score} (Player1)`;
-            }
-          }
-        });
-      }
+      playerSubmitFunction(
+        player1Submit,
+        player2Input,
+        player1Input,
+        "player1"
+      );
+    });
+  }
+});
+
+playAgain.addEventListener("click", function () {
+  player1Input.value = null
+  player2Input.value = null
+  console.log(player1Input.value);
+  console.log(player2Input.value);
+  guess.textContent = "Start Guess A Number Btween 1 to 20";
+  playerScore.textContent = "Score = 20";
+  screen.textContent = "?";
+  player1Input.value = "";
+  player2Input.value = "";
+  score = 20;
+  playerHighScrore = 0;
+  const randomNumber = Math.trunc(Math.random() * 2) + 1;
+  // const randomNumber = 2;
+  // console.log(randomNumber);
+  if (randomNumber === 1) {
+    player1Input.disabled = false;
+    player2Input.disabled = true;
+    player1Submit.addEventListener("click", function () {
+        playerSubmitFunction(
+          player2Submit,
+          player1Input,
+          player2Input,
+          "player2"
+        );
+      });
+  } else {
+    player2Input.disabled = false;
+    // player1Input.value = "";
+    player1Input.disabled = true;
+    player2Submit.addEventListener("click", function () {
+      playerSubmitFunction(
+        player1Submit,
+        player2Input,
+        player1Input,
+        "player1"
+      );
     });
   }
 });
